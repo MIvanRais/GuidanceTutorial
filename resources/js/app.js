25d0c4fa2529707602beabc77385a170e3edb4e9
MIvanRais <26ivanrais@gmail.com>
2022-10-14 04:48:27 +0700

    Add Style At 992px - 1200px Breakpoint

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,3 +11,35 @@ closeBtn.addEventListener('click', function () {
     active.style.display = 'none';
 })
 /* ... */
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 50,
+    slidesPerGroup: 1,
+    loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: true,
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // 0: {
+        //     slidesPerView: 1,
+        // },
+        // 520: {
+        //     slidesPerView: 2,
+        // },
+        992: {
+            slidesPerView: 3,
+        },
+        1201: {
+            slidesPerView: 5,
+        },
+    },
+});
