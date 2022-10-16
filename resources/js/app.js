0a73987352ad22eefd5373f41e1927605393c665
MIvanRais <26ivanrais@gmail.com>
2022-10-16 21:15:01 +0700

    Add Syllabus On Tutorial Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,69 @@ closeBtn.addEventListener('click', function () {
 })
 /* ... */
 
+// Syllabus
+// Get the modal
+let modal = document.querySelector('#myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let modalImg = document.querySelector('#img01');
+let captionText = document.querySelector('#caption');
+
+const buttons = document.querySelectorAll('.butt');
+buttons.forEach((but) => {
+    but.addEventListener('click', function () {
+        if (but === buttons[0]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-linux.png';
+            captionText.innerHTML = 'Syllabus Linux';
+        }
+        else if (but === buttons[1]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-gh.png';
+            captionText.innerHTML = 'Syllabus Git & Github';
+        }
+        else if (but === buttons[2]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-html.png';
+            captionText.innerHTML = 'Syllabus HTML';
+        }
+        else if (but === buttons[3]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-css.png';
+            captionText.innerHTML = 'Syllabus CSS';
+        }
+        else if (but === buttons[4]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-js.png';
+            captionText.innerHTML = 'Syllabus JavaScript';
+        }
+        else if (but === buttons[5]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-dom.png';
+            captionText.innerHTML = 'Syllabus JavaScript DOM';
+        }
+        else if (but === buttons[6]) {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-java.png';
+            captionText.innerHTML = 'Syllabus Java';
+        }
+        else {
+            modal.style.display = 'block';
+            modalImg.src = 'resources/img/roadmap-jsp.png';
+            captionText.innerHTML = 'Syllabus Servlet & JSP';
+        }
+    });
+});
+
+// Get the <span> element that closes the modal
+let span = document.querySelector('.close');
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener('click', function () {
+    modal.style.display = 'none';
+})
+/* ... */
+
 // Swiper
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 5,
