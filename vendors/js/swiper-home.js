7f227423e5a86ffbb0fc618bbd899c416a2ad60a
MIvanRais <26ivanrais@gmail.com>
2023-03-25 13:20:13 +0700

    Add Some Behaviors Of The Category Slider On Phone Viewport

--- vendors/js/swiper-home.js
+++ vendors/js/swiper-home.js
@@ -20,3 +20,14 @@ const recommendedSwiper = new Swiper('.recommended-swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+const categorySliderSwiper = new Swiper('.category-slider-swiper', {
+    // Optional parameters
+    slidesPerView: 2,
+    spaceBetween: 8,
+    slidesPerGroup: 1,
+    // loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: false,
+    grabCursor: 'true',
+});
