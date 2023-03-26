745ac4705b07b31b3ba483e982e8878964a69ea0
MIvanRais <26ivanrais@gmail.com>
2023-03-26 13:32:14 +0700

    Manipulates The Login Form In Home Page On Phone Viewport

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,17 +1,34 @@
 // **modal burger menu //
 const burgerMenuBtn = document.querySelector("#open-menu");
 const modalMenu = document.querySelector("#modal-menu");
-const closeBtn = document.querySelector("#close-btn");
+const closeMenuBtn = document.querySelector(".modal-menu #close-btn");
 const header = document.querySelector("header");
 
-burgerMenuBtn.addEventListener('click',() => {
-   modalMenu.showModal();
-   document.body.classList.add("scroll-hidden");
-   header.style.marginTop = "0px";
+burgerMenuBtn.addEventListener('click', () => {
+    modalMenu.showModal();
+    document.body.classList.add("scroll-hidden");
+    header.style.marginTop = "0px";
 })
 
-closeBtn.addEventListener('click', () => {
+closeMenuBtn.addEventListener('click', () => {
     modalMenu.close();
     document.body.classList.remove("scroll-hidden");
     header.style.marginTop = "22px";
 })
+// **--------------//
+
+// **modal login //
+const loginBtn = document.querySelector("#open-modal-login");
+const modalLogin = document.querySelector("#modal-login");
+const closeLogin = document.querySelector(".modal-login #close-btn")
+
+loginBtn.addEventListener('click', () => {
+    modalLogin.showModal();
+    document.body.classList.add("scroll-hidden");
+})
+
+closeLogin.addEventListener('click', () => {
+    modalLogin.close();
+    document.body.classList.remove("scroll-hidden");
+})
+// **--------------//
