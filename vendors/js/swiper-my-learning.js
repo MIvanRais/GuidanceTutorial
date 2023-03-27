656163ff8c513bd1504f6f495bb3c82f14606d38
MIvanRais <26ivanrais@gmail.com>
2023-03-27 16:08:23 +0700

    Add Some Behaviors Of The Category Slider On Phone Viewport

---
+++ vendors/js/swiper-my-learning.js
@@ -0,0 +1,10 @@
+const categorySliderSwiper = new Swiper('.category-slider-swiper', {
+    // Optional parameters
+    slidesPerView: 2,
+    spaceBetween: 8,
+    slidesPerGroup: 1,
+    // loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: false,
+    grabCursor: 'true',
+});
