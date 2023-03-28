1a27941fe8f5e934d10c2b81bf27cb0932fc19f7
MIvanRais <26ivanrais@gmail.com>
2023-03-28 09:41:00 +0700

    Manipulates The Request Form In Request Status Page On Phone Viewport

--- resources/js/request-status.js
+++ resources/js/request-status.js
@@ -15,3 +15,19 @@ for (let requestStatus = 0; requestStatus <= requestStatuses.length - 1; request
         eachRows[requestStatus].classList.add('border-status-new');
     }
 }
+
+// **modal request //
+const requestBtn = document.querySelector("footer>button");
+const modalRequest = document.querySelector("#modal-request");
+const closeRequestBtn = document.querySelector("#modal-request #close-btn");
+
+requestBtn.addEventListener('click', () => {
+    modalRequest.showModal();
+    document.body.classList.add("scroll-hidden");
+})
+
+closeRequestBtn.addEventListener('click', () => {
+    modalRequest.close();
+    document.body.classList.remove("scroll-hidden");
+})
+// **--------------//
