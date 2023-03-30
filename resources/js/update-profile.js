e0497f0671e37c6bc57119ea8236d8727e8fc76c
MIvanRais <26ivanrais@gmail.com>
2023-03-30 11:26:19 +0700

    Manipulate The Image Element According What You Uploaded

--- resources/js/update-profile.js
+++ resources/js/update-profile.js
@@ -18,4 +18,23 @@ for (let input = 0; input < inputs.length; input++) {
 }
 // **-----------------------------
 
+// **upload an image interactively
+const inputFile = document.querySelector('input[type="file"]');
+const imgName = document.querySelector('.photo-profile-field>p');
+const previewImg = document.querySelector('img');
 
+function updateImageDisplay() {
+    // **stores 'files' object in the file variable.
+    const file = inputFile.files;
+
+    // **check an user already to upload an image
+    if (file.length === 1) {
+        // **Change the content inside the p tag according the selected image
+        imgName.innerText = file[0].name;
+        // **Preview the image you uploaded
+        previewImg.setAttribute('src', URL.createObjectURL(file[0]));
+    }
+}
+
+inputFile.addEventListener('change', updateImageDisplay);
+// **-----------------------------
